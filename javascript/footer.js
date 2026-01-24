8ad89b58db105703557e06ce5d4b2dc9d274a023
salimshohan <salimshohan32@gmail.com>
2026-01-24 23:40:49 +0600

    Update footer.js

--- javascript/footer.js
+++ javascript/footer.js
@@ -1,2 +1,9 @@
 let footer = document.getElementById("footer");
-footer.innerHTML = "";
+footer.innerHTML = '<style>footer {
+    width: 100%; padding: 15px; text-align: center;
+    background: #1f2937;
+    border-top: 1px solid #dadada;
+    margin-top: auto; font-size: 14px; color: white;
+}</style>
+
+<footer>&copy;' + year + ' Interactive Learning<br>All rights reserved.</footer>';
