bf952434a0951c909dc5119d48563eae82e00220
salimshohan <salimshohan32@gmail.com>
2026-01-25 00:31:54 +0600

    Update footer.js

--- javascript/footer.js
+++ javascript/footer.js
@@ -4,16 +4,6 @@
 
   const year = new Date().getFullYear();
 
-  footer.style.cssText = `
-    width:100%;
-    padding:15px;
-    text-align:center;
-    background:#1f2937;
-    border-top:1px solid #dadada;
-    font-size:14px;
-    color:white;
-  `;
-
   footer.innerHTML = `
     <div class="site-footer">
       &copy; ${year} Interactive Learning<br>
