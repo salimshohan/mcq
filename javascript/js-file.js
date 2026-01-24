0b7b311293523aa64dcf981124742fa4674af7da
salimshohan <salimshohan32@gmail.com>
2026-01-25 00:42:38 +0600

    Update js-file.js

--- javascript/js-file.js
+++ javascript/js-file.js
@@ -1,4 +1,10 @@
-const script = document.createElement('script');
-script.src = '/javascript/footer.js';
-script.type = 'text/javascript';
-document.body.appendChild(script);
+function loadScript(url) {
+    const script = document.createElement('script');
+    script.src = url;
+    script.type = 'text/javascript';
+    document.body.appendChild(script);
+}
+
+// You may add js file here.
+loadScript('/javascript/header.js');
+loadScript('/javascript/footer.js');
