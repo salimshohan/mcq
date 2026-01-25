46eb85208fd994fb4fd7fb7a92171274cc87c61e
salimshohan <salimshohan32@gmail.com>
2026-01-25 08:36:35 +0600

    Update js-file.js

--- javascript/js-file.js
+++ javascript/js-file.js
@@ -1,13 +1,1 @@
-// 1. Load Header Script
-const headerScript = document.createElement('script');
-headerScript.src = '/javascript/header.js';
-headerScript.type = 'text/javascript';
-headerScript.defer = true; // HTML লোড হওয়া পর্যন্ত অপেক্ষা করবে
-document.body.appendChild(headerScript);
-
-// 2. Load Footer Script
-const footerScript = document.createElement('script');
-footerScript.src = '/javascript/footer.js';
-footerScript.type = 'text/javascript';
-footerScript.defer = true; // HTML লোড হওয়া পর্যন্ত অপেক্ষা করবে
-document.body.appendChild(footerScript);
+document.write("<script src='/javasctipt/footer.js'></script>");
